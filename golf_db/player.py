62e0ec9cb3d42ea4d3d7993bb83f775aaa10584e
Steve Journeay <sjournea@taralaur.com>
2017-03-18 15:31:47 -0700

    Added validate() to GolfPlayer

--- golf_db/player.py
+++ golf_db/player.py
@@ -14,6 +14,22 @@ class GolfPlayer(Doc):
   """
   fields = ['email', 'first_name', 'last_name', 'nick_name', 'handicap']
 
+  def validate(self):
+    if self.email is None:
+      raise DocValidateFail('email must be defined')
+    if not isinstance(self.email, str):
+      raise DocValidateFail('email must be a string')
+    if self.first_name is not None and not isinstance(self.first_name, str):
+      raise DocValidateFail('first_name must be a string')
+    if self.last_name is not None and not isinstance(self.last_name, str):
+      raise DocValidateFail('last_name must be a string')
+    if self.nick_name is not None and not isinstance(self.nick_name, str):
+      raise DocValidateFail('nick_name must be a string')
+    if self.handicap is None:
+      raise DocValidateFail('handicap must be defined.')
+    if not isinstance(self.handicap, float):
+      raise DocValidateFail('handicap must be a float')
+      
   def __str__(self):
     return '{:<15} - {:<6} {:<10} ({:<6}) handicap {:.1f}'.format(self.email, self.first_name, self.last_name, self.nick_name, self.handicap)
   
--- test/test_player.py
+++ test/test_player.py
@@ -8,16 +8,18 @@ class GolfPlayerInitCase(unittest.TestCase):
 
   def test_init_empty(self):
     # check default parameters
-    player = GolfPlayer()
-    self.assertIsNone(player.email)
+    player = GolfPlayer(email='sjournea@gmail.com', handicap=20.0)
+    player.validate()
+    self.assertEqual(player.email, 'sjournea@gmail.com')
     self.assertIsNone(player.last_name)
     self.assertIsNone(player.first_name)
     self.assertIsNone(player.nick_name)
-    self.assertIsNone(player.handicap)
+    self.assertEqual(player.handicap, 20)
 
   def test_init_from_dict(self):
     for dct in GolfPlayers:
       player = GolfPlayer(dct=dct)
+      player.validate()
       self.assertEqual(dct['last_name'], player.last_name)
       self.assertEqual(dct['first_name'], player.first_name)
       self.assertEqual(dct['nick_name'], player.nick_name)
@@ -27,13 +29,15 @@ class GolfPlayerInitCase(unittest.TestCase):
 class GolfPlayerDictCase(unittest.TestCase):
   def test_toDict(self):
     for dct in GolfPlayers:
-      player = GolfPlayer(dct=dct) 
+      player = GolfPlayer(dct=dct)
+      player.validate()
       self.assertEqual(player.toDict(), dct)
     
   def test_fromDict(self):
     for dct in GolfPlayers:
       player = GolfPlayer()
       player.fromDict(dct)
+      player.validate()
       self.assertEqual(player.toDict(), dct)
 
   def test_equalOperator(self):
