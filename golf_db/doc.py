ace3b996e5a193502379726c6756de4aedf99375
Steve Journeay <sjournea@taralaur.com>
2017-03-18 12:59:04 -0700

    Cleaned up doc.py and added more tests to test_doc.py

--- golf_db/doc.py
+++ golf_db/doc.py
@@ -13,6 +13,7 @@ class DB(object):
 
 class Doc(object):
   """Abstract fields MUST be defined."""
+  fields = []
   def __init__(self, dct=None):
     super(Doc, self).__init__()
     # initiaze all fields to None
@@ -42,29 +43,3 @@ class Doc(object):
     return not self == other
 
 
-class Expand(object):
-  """Abstract fields MUST be defined."""
-  def __init__(self, dct=None):
-    super(Expand, self).__init__()
-    # initialize from dictionary
-    if dct:
-      self.fromDict(dct)
-  
-  def fromDict(self, dct):
-    for key,value in dct.iteritems():
-      setattr(self, key, value)
-  
-  #def toDict(self):
-    #dct = {}
-    #for field in self.fields:
-      #dct[field] = getattr(self, field)
-    #return dct
-    
-  #def __eq__(self, other):
-    #for field in self.fields:
-      #if getattr(self, field) != getattr(other, field):
-        #return False
-    #return True
-
-  def __ne__(self,other):
-    return not self == other
--- test/test_doc.py
+++ test/test_doc.py
@@ -1,37 +1,103 @@
 import unittest
 
-from golf_db.doc import Doc, Expand
+from golf_db.doc import Doc
 
-#class DocInitCase(unittest.TestCase):
-
-  #def test_init_empty(self):
-    ## check default parameters
-    #doc = Doc()
-    #print doc
-    ##self.assertIsNone()
-    ##self.assertIsNone(course.holes)
-
-class ExpandInitCase(unittest.TestCase):
-
+class DocBaseCase(unittest.TestCase):
   def test_init_empty(self):
-    # check default parameters
-    e = Expand()
-    self.assertDictEqual(e.__dict__, {})
+    doc = Doc()
+    self.assertEqual(doc.toDict(), {})
+    doc.fromDict({})
+    self.assertEqual(doc.toDict(), {})
 
-  def test_init_dict_int(self):
-    # check default parameters
-    e = Expand(dct={'i': 1, 'j':100})
-    self.assertEqual(e.i, 1)
-    self.assertEqual(e.j, 100)
+  def test_init_dct(self):
+    doc = Doc(dct={})
+    self.assertEqual(doc.toDict(), {})
+    doc.fromDict({})
+    self.assertEqual(doc.toDict(), {})
 
-  def test_init_dict_float(self):
-    e = Expand(dct={'f': 1.0})
-    self.assertEqual(e.f, 1.0)
-    e = Expand(dct={'f': 1.5e-99})
-    self.assertEqual(e.f, 1.5e-99)
 
-  def test_init_dict_string(self):
-    e = Expand(dct={'s': 'String'})
-    self.assertEqual(e.s, 'String')
-    e = Expand(dct={'s': 'Hello Python'})
-    self.assertEqual(e.s, 'Hello Python')
+class TestDoc(Doc):
+  fields = ['ivalue', 'fvalue', 'svalue', 'lvalue']
+
+class DocFieldEmpty(unittest.TestCase):
+  def test_init(self):
+    t = TestDoc()
+    for field in TestDoc.fields:
+      self.assertTrue(hasattr(t, field))
+      self.assertIsNone(getattr(t, field))
+
+  def test_init_empty_dict(self):
+    t = TestDoc(dct={})
+    for field in TestDoc.fields:
+      self.assertTrue(hasattr(t, field))
+      self.assertIsNone(getattr(t, field))
+
+  def test_dct_is_nones(self):
+    t = TestDoc()
+    dct = t.toDict()
+    for field in TestDoc.fields:
+      self.assertIn(field, dct)
+      self.assertIsNone(dct[field])
+    self.assertDictEqual(dct, {field: None for field in TestDoc.fields})
+
+
+class DocFieldValues(unittest.TestCase):
+  def test_ivalue(self):
+    lst = [1, 10, 100, 1000, 0, -1]
+    for value in lst:
+      t = TestDoc(dct={'ivalue':value})
+      self.assertEqual(t.ivalue, value)
+      self.assertIsNone(t.fvalue)
+      self.assertIsNone(t.svalue)
+      self.assertIsNone(t.lvalue)
+      dct = t.toDict()
+      self.assertEqual(len(dct), len(TestDoc.fields))
+      self.assertEqual(dct['ivalue'], value)
+      self.assertIsNone(dct['fvalue'])
+      self.assertIsNone(dct['svalue'])
+      self.assertIsNone(dct['fvalue'])
+
+  def test_fvalue(self):
+    lst = [1.0, 10.0, 100.0, 1e-6, 0, -1]
+    for value in lst:
+      t = TestDoc(dct={'fvalue':value})
+      self.assertEqual(t.fvalue, value)
+      self.assertIsNone(t.ivalue)
+      self.assertIsNone(t.svalue)
+      self.assertIsNone(t.lvalue)
+      dct = t.toDict()
+      self.assertEqual(len(dct), len(TestDoc.fields))
+      self.assertEqual(dct['fvalue'], value)
+      self.assertIsNone(dct['ivalue'])
+      self.assertIsNone(dct['svalue'])
+      self.assertIsNone(dct['fvalue'])
+
+  def test_svalue(self):
+    lst = ['hello', 'world', '', '\x46']
+    for value in lst:
+      t = TestDoc(dct={'svalue':value})
+      self.assertEqual(t.svalue, value)
+      self.assertIsNone(t.ivalue)
+      self.assertIsNone(t.fvalue)
+      self.assertIsNone(t.lvalue)
+      dct = t.toDict()
+      self.assertEqual(len(dct), len(TestDoc.fields))
+      self.assertEqual(dct['svalue'], value)
+      self.assertIsNone(dct['ivalue'])
+      self.assertIsNone(dct['fvalue'])
+      self.assertIsNone(dct['lvalue'])
+
+  def test_fvalue(self):
+    lst = [True, False]
+    for value in lst:
+      t = TestDoc(dct={'lvalue':value})
+      self.assertEqual(t.lvalue, value)
+      self.assertIsNone(t.ivalue)
+      self.assertIsNone(t.svalue)
+      self.assertIsNone(t.fvalue)
+      dct = t.toDict()
+      self.assertEqual(len(dct), len(TestDoc.fields))
+      self.assertEqual(dct['lvalue'], value)
+      self.assertIsNone(dct['ivalue'])
+      self.assertIsNone(dct['fvalue'])
+      self.assertIsNone(dct['svalue'])
