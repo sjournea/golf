4fd376109d19768938439566fe095cfbebed5fef
Steve Journeay <sjournea@taralaur.com>
2017-04-02 13:41:12 -0700

    Added python ABCMeta usage for abstract class GolfGame
    * methods that MUST be deined:
      start(), addScore(), getScorecard(), getLeaderboard()

--- golf_db/game.py
+++ golf_db/game.py
@@ -1,8 +1,10 @@
 """ game.py - GolfGame class."""
+from abc import ABCMeta, abstractmethod
 from .exceptions import GolfException
 
 class GolfGame(object):
   """Base class for all golf games."""
+  __metaclass__=ABCMeta
   def __init__(self, golf_round, scores, options=None):
     self.golf_round = golf_round
     self.scores = scores
@@ -12,18 +14,22 @@ class GolfGame(object):
     """Validate a game."""
     pass
 
+  @abstractmethod
   def start(self):
     """Start the game."""
     pass
 
+  @abstractmethod
   def addScore(self, hole_index, lstScores):
     """add scores for a hole."""
     pass
 
+  @abstractmethod
   def getScorecard(self, **kwargs):
     """Return scorecard dictionary for this game."""
     pass
   
+  @abstractmethod
   def getLeaderboard(self, **kwargs):
     """Return leaderboard dictionary for this game."""
     pass
--- test/test_game.py
+++ test/test_game.py
@@ -2,10 +2,10 @@ import unittest
 import datetime
 
 from golf_db.round import GolfRound
-from golf_db.test_data import GolfRounds,GolfCourses, GolfPlayers
+#from golf_db.test_data import GolfRounds,GolfCourses, GolfPlayers
 from golf_db.db import GolfDB
 from golf_db.game import GolfGame
-from golf_db.exceptions import GolfException
+#from golf_db.exceptions import GolfException
 
 class GolfGameTest(unittest.TestCase):
   @classmethod
@@ -26,19 +26,9 @@ class GolfGameTest(unittest.TestCase):
       pl = self.db.playerFind(email)
       self.gr.addPlayer(pl, tee_name)
       
-  def test_base_init(self):
-    g = GolfGame(self.gr, self.gr.scores)
-    self.assertEqual(g.golf_round, self.gr)
-    self.assertEqual(g.scores, self.gr.scores)
-    self.assertEqual(g.options, {})
+  def test_base_abstract(self):
+    with self.assertRaises(TypeError):
+      g = GolfGame(self.gr, self.gr.scores)
     
-  def test_base_methods(self):
-    g = GolfGame(self.gr, self.gr.scores)
-    g.validate()
-    g.start()
-    g.addScore(0, [])
-    g.getScorecard()
-    g.getLeaderboard()
-    g.complete()
 
 
