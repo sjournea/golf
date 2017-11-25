6d612bb77eb88428294b0dd4e3f48926c617aa77
Steve Journeay <sjournea@taralaur.com>
2017-11-24 19:47:46 -0800

    Added stableford.

---
+++ test/test_game_stableford.py
@@ -0,0 +1,127 @@
+import unittest
+import datetime
+
+from golf_db.round import GolfRound
+from golf_db.course import GolfCourse
+from golf_db.test_data import TestGolfPlayers
+from golf_db.db import GolfDBAdmin
+from golf_db.game_stableford import StablefordGame
+from golf_db.player import GolfPlayer
+from golf_db.exceptions import GolfException
+
+
+class GolfStablefordGameTest(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    cls.db = GolfDBAdmin(database='golf_game_test')
+    cls.db.create()
+    
+  def setUp(self):
+    course_name = 'Canyon Lakes'
+    tee_name = 'Blue'
+    date_of_round = datetime.datetime(2017, 3, 23)
+    
+    self.gr = GolfRound()
+    self.gr.course = self.db.courseFind(course_name, dbclass=GolfCourse)[0]
+    self.gr.date = date_of_round
+    for dct in TestGolfPlayers:
+      pl = GolfPlayer(dct=dct)
+      self.gr.addPlayer(pl, tee_name)
+
+  def test_game_init(self):
+    g = StablefordGame(self.gr, self.gr.scores)
+    self.assertEqual(g.stableford_type, 'Classic')
+
+    g = StablefordGame(self.gr, self.gr.scores, stableford_type='British')
+    self.assertEqual(g.stableford_type, 'British')
+    
+  def test_game_start(self):
+    g = StablefordGame(self.gr, self.gr.scores)
+    g.start()
+    for pl in g.scores:
+      self.assertEquals(pl.net['score'], 18*[None])
+      self.assertEquals(pl.points['point'], 18*[None])
+      self.assertEquals(pl.points['in'], 0)
+      self.assertEquals(pl.points['out'], 0)
+      self.assertEquals(pl.points['total'], 0)
+      
+  def test_game_add_score(self):
+    g = StablefordGame(self.gr, self.gr.scores)
+    g.start()
+    g.addScore(0, [4,4,4,4])
+    self.assertEqual(g.scores[0].points['point'][0], 2)
+    self.assertEqual(g.scores[1].points['point'][0], 2)
+    self.assertEqual(g.scores[2].points['point'][0], 2)
+    self.assertEqual(g.scores[3].points['point'][0], 2)
+    g.addScore(1, [3,4,5,6])
+    self.assertEqual(g.scores[0].points['point'][1], 8)
+    self.assertEqual(g.scores[1].points['point'][1], 5)
+    self.assertEqual(g.scores[2].points['point'][1], 2)
+    self.assertEqual(g.scores[3].points['point'][1], 0)
+    g.addScore(2, [3,3,4,4])
+    self.assertEqual(g.scores[0].points['point'][2], 2)
+    self.assertEqual(g.scores[1].points['point'][2], 2)
+    self.assertEqual(g.scores[2].points['point'][2], 0)
+    self.assertEqual(g.scores[3].points['point'][2], 0)
+    
+  #def test_game_scorecard(self):
+    #g = SixPointGame(self.gr, self.gr.scores)
+    #g.start()
+    #dct = g.getScorecard()
+    #self.assertIn('course', dct)
+    #self.assertIn('header', dct)
+    #self.assertIn('players', dct)
+    #players = dct['players']
+    #for player in players:
+      #self.assertIn('line', player)
+      #self.assertEqual(player['in'], 0)
+      #self.assertEqual(player['out'], 0)
+      #self.assertEqual(player['total'], 0)
+      
+    #g.addScore(0, [4,4,4])
+    #dct = g.getScorecard()
+    #self.assertIn('course', dct)
+    #self.assertIn('header', dct)
+    #self.assertIn('players', dct)
+    #for player in dct['players']:
+      #self.assertIn('line', player)
+      #self.assertEqual(player['in'], 0)
+      #self.assertEqual(player['out'], 2)
+      #self.assertEqual(player['total'], 2)
+
+    #g.addScore(1, [3,4,4])
+    #dct = g.getScorecard()
+    #self.assertIn('course', dct)
+    #self.assertIn('header', dct)
+    #self.assertIn('players', dct)
+    #for player in dct['players']:
+      #self.assertIn('line', player)
+      #self.assertIn('in', player)
+      #self.assertIn('out', player)
+      #self.assertIn('total', player)
+
+  #def test_game_leaderboard(self):
+    #g = SixPointGame(self.gr, self.gr.scores)
+    #g.start()
+    #dct = g.getLeaderboard()
+    #self.assertIn('hdr', dct)
+    #self.assertIn('leaderboard', dct)
+    #players = dct['leaderboard']
+    #for player in players:
+      #self.assertIn('player', player)
+      #self.assertIn('total', player)
+      #self.assertIn('line', player)
+      #self.assertIn('pos', player)
+      #self.assertIn('thru', player)
+
+    #g.addScore(1, [4,4,4])
+    #dct = g.getLeaderboard()
+    #self.assertIn('hdr', dct)
+    #self.assertIn('leaderboard', dct)
+    #players = dct['leaderboard']
+    #for player in players:
+      #self.assertIn('player', player)
+      #self.assertIn('total', player)
+      #self.assertIn('line', player)
+      #self.assertIn('pos', player)
+      #self.assertIn('thru', player)
