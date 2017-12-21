8e2f9fb30b6c755c6d2a3b9eb0afcf81ff2fbfa7
Steve Journeay <sjournea@taralaur.com>
2017-12-21 16:26:10 -0700

    Added wager to snake game

--- golf_db/game_snake.py
+++ golf_db/game_snake.py
@@ -16,16 +16,25 @@ class SnakeGame(PuttGame):
     """Start the game."""
     for pl in self.scores:
       # putts
-      pl._three_putts = [None for _ in range(len(self.golf_round.course.holes))]
-      pl._in = 0
-      pl._out = 0
-      pl._total = 0
+      pl.dct_points = {
+        'holes': [None for _ in range(len(self.golf_round.course.holes))],
+        'in': 0,
+        'out': 0,
+        'total': 0,
+      }
+      if self._wager:
+        pl.dct_money = {
+          'holes': [None for _ in range(len(self.golf_round.course.holes))],
+          'in': 0.0,
+          'out': 0.0,
+          'total': 0.0,
+        }
     
     self._has_snake = None
     # update header to scorecard
     title = ' Snake - {} '.format(self._snake_type)
     self.dctScorecard['header'] = '{0:*^98}'.format(title)
-    self.dctLeaderboard['hdr'] = 'Pos Name   Snakes Thru'
+    self.dctLeaderboard['hdr'] = 'Pos Name  {:>6} Thru'.format('Money' if self._wager else 'Points')
   
   def setOptions(self, options):
     """Additional options parsed by each test."""
@@ -43,7 +52,7 @@ class SnakeGame(PuttGame):
       snake_winner = None
       lst_three_putts = []
       for gs, putt in zip(self.scores, lstPutts):
-        gs._three_putts[index] = 0
+        gs.dct_points[index] = 0
         if putt > 2:
           lst_three_putts.append((gs, putt))
       if len(lst_three_putts) > 1:
@@ -64,8 +73,18 @@ class SnakeGame(PuttGame):
       if snake_winner:
         if self._has_snake and self._has_snake == snake_winner:
           # immediate payout and release snake
-          self._has_snake._three_putts[index] = -1
+          self._has_snake.dct_points['holes'][index] = -1
+          self._update_totals(self._has_snake.dct_points)
           self._has_snake = None
+          if self._wager:
+            # payout all other players
+            # pay all other players the wager
+            for sc in self.scores:
+              if sc == snake_winner:
+                sc.dct_money['holes'][index] = -self._wager*(len(self.scores)-1)
+              else:
+                sc.dct_money['holes'][index] = self._wager
+              self._update_totals(sc.dct_money)
         else:
           self._has_snake = snake_winner
       # snake automatic payout on 9 and 18
@@ -74,29 +93,26 @@ class SnakeGame(PuttGame):
         self._has_snake = None
     else:
       for gs, putt in zip(self.scores, lstPutts):
-        gs._three_putts[index] = -1 if putt > 2 else 0
+        if putt > 2:
+          gs.dct_points['holes'][index] = -1 
+          self._update_totals(gs.dct_points)
     
-    for gs in self.scores:
-      gs._out = sum([sc for sc in gs._three_putts[:9] if isinstance(sc, int)])
-      gs._in = sum([sc for sc in gs._three_putts[9:] if isinstance(sc, int)])
-      gs._total = gs._in + gs._out
-
   def getScorecard(self, **kwargs):
     """Scorecard with all players."""
     lstPlayers = []
     for n,score in enumerate(self.scores):
       dct = {'player': score.player }
-      dct['in'] = score._in
-      dct['out'] = score._out
-      dct['total'] = score._total
+      dct['in'] = score.dct_points['in']
+      dct['out'] = score.dct_points['out']
+      dct['total'] = score.dct_points['total']
       # build line for stdout
       line = '{:<6}'.format(score.player.nick_name)
-      for putt in score._three_putts[:9]:
+      for putt in score.dct_points['holes'][:9]:
         line += ' {:>3}'.format(putt) if putt is not None else '    '
-      line += ' {:>4}'.format(score._out)
-      for putt in score._three_putts[9:]:
+      line += ' {:>4}'.format(dct['out'])
+      for putt in score.dct_points['holes'][9:]:
         line += ' {:>3}'.format(putt) if putt is not None else '    '
-      line += ' {:>4} {:>4}'.format(score._in, score._total)
+      line += ' {:>4} {:>4}'.format(dct['in'], dct['total'])
       dct['line'] = line
       lstPlayers.append(dct)
     self.dctScorecard['players'] = lstPlayers
@@ -105,33 +121,39 @@ class SnakeGame(PuttGame):
   def getLeaderboard(self, **kwargs):
     """Scorecard with all players."""
     board = []
-    scores = sorted(self.scores, key=lambda score: score._total, reverse=True)
+    scores = sorted(self.scores, key=lambda score: score.dct_points['total'], reverse=True)
     pos = 1
     prev_total = None
     for score in scores:
       score_dct = {
         'player': score.player,
-        'total' : score._total,
+        'total' : score.dct_points['total'],
+        'money' : score.dct_money['total'] if self._wager else None,
       }
       if prev_total != None and score_dct['total'] > prev_total:
         pos += 1
       prev_total = score_dct['total']
       score_dct['pos'] = pos
-      for n,putt in enumerate(score._three_putts):
+      for n,putt in enumerate(score.dct_points['holes']):
         if putt is None:
           break
       else:
         n += 1
       score_dct['thru'] = n
-      score_dct['line'] = '{:<3} {:<6} {:>5} {:>4}'.format(
-        score_dct['pos'], score_dct['player'].nick_name, score_dct['total'], score_dct['thru'])
+      if self._wager:
+        money = '--' if score_dct['money'] == 0.0 else '${:<2g}'.format(score_dct['money'])
+        score_dct['line'] = '{:<3} {:<6} {:^5} {:>4}'.format(
+          score_dct['pos'], score_dct['player'].nick_name, money, score_dct['thru'])
+      else:
+        score_dct['line'] = '{:<3} {:<6} {:>5} {:>4}'.format(
+          score_dct['pos'], score_dct['player'].nick_name, score_dct['total'], score_dct['thru'])
       board.append(score_dct)
     self.dctLeaderboard['leaderboard'] = board
     return self.dctLeaderboard
 
   def getStatus(self, **kwargs):
     """Scorecard with all players."""
-    for n,putt in enumerate(self.scores[0]._three_putts):
+    for n,putt in enumerate(self.scores[0].dct_points['holes']):
       if putt is None:
         self.dctStatus['next_hole'] = n+1
         line = ''
