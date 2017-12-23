388f8e6eb6c4bdabae73348e799ff440388aad5e
Steve Journeay <sjournea@taralaur.com>
2017-12-23 11:15:13 -0700

    Added dict to putts/snake games

--- golf_db/game_snake.py
+++ golf_db/game_snake.py
@@ -16,20 +16,8 @@ class SnakeGame(PuttGame):
     """Start the game."""
     for pl in self.scores:
       # putts
-      pl.dct_points = {
-        'holes': [None for _ in range(len(self.golf_round.course.holes))],
-        'in': 0,
-        'out': 0,
-        'total': 0,
-      }
-      if self._wager:
-        pl.dct_money = {
-          'holes': [None for _ in range(len(self.golf_round.course.holes))],
-          'in': 0.0,
-          'out': 0.0,
-          'total': 0.0,
-        }
-    
+      pl.dct_points = self._init_dict()
+      pl.dct_money = self._init_dict(score_type=float) if self._wager else None
     self._has_snake = None
     self._thru = 0
     # update header to scorecard
