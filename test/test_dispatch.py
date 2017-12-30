26145bdd5c100329eb8a350b13772d464db24e0f
Steve Journeay <sjournea@taralaur.com>
2017-12-29 19:48:03 -0700

    Moved dispatch testing to class.

--- test/test_dispatch.py
+++ test/test_dispatch.py
@@ -13,55 +13,52 @@ class DispatchInstanceTest(unittest.TestCase):
     self.assertEqual(pd.pathmap, {})
 
     
-hello_resp = """
-<html>
-  <head>
-    <title>Hello {name}</title>
-  </head>
-  <body>
-    <h1>Hello {name}!</h1>
-  </body>
-</html>
-"""
-
-def hello_world(environ, start_response):
-  start_response('200 OK', [('Content-type', 'text/html')])
-  params = environ['params']
-  resp = hello_resp.format(name=params.get('name'))
-  yield resp.encode('utf-8')
-  
-localtime_resp = '''\
-<?xml version="1.0"?>
-<time>
-  <year>{t.tm_year}</year>
-  <month>{t.tm_mon}</month>
-  <day>{t.tm_mday}</day>
-  <hour>{t.tm_hour}</hour>
-  <minute>{t.tm_min}</minute>
-  <second>{t.tm_sec}</second>
-</time>
-'''
-
-def localtime(environ, start_response):
-  start_response('200 OK', [('Content-type', 'application/xml')])
-  resp = localtime_resp.format(t=time.localtime())
-  yield resp.encode('utf-8')
-
 def config_register(dispatcher):
   dispatcher.register('GET', '/hello', hello_world)
   dispatcher.register('GET', '/localtime', localtime)
 
 
 class DispatchRunTest(unittest.TestCase):
+  hello_resp = """
+  <html>
+    <head>
+      <title>Hello {name}</title>
+    </head>
+    <body>
+      <h1>Hello {name}!</h1>
+    </body>
+  </html>
+  """
   
-  def setUp(self):
-    pass
+  def hello_world(self, environ, start_response):
+    start_response('200 OK', [('Content-type', 'text/html')])
+    params = environ['params']
+    resp = self.hello_resp.format(name=params.get('name'))
+    yield resp.encode('utf-8')
+    
+  localtime_resp = '''\
+  <?xml version="1.0"?>
+  <time>
+    <year>{t.tm_year}</year>
+    <month>{t.tm_mon}</month>
+    <day>{t.tm_mday}</day>
+    <hour>{t.tm_hour}</hour>
+    <minute>{t.tm_min}</minute>
+    <second>{t.tm_sec}</second>
+  </time>
+  '''
+  
+  def localtime(self, environ, start_response):
+    start_response('200 OK', [('Content-type', 'application/xml')])
+    resp = self.localtime_resp.format(t=time.localtime())
+    yield resp.encode('utf-8')
+
   
   def test_register(self):
     pd = PathDispatcher()
     paths = (
-      ('GET', '/hello', hello_world),
-      ('GET', '/localtime', localtime),
+      ('GET', '/hello', self.hello_world),
+      ('GET', '/localtime', self.localtime),
     )
     self.assertEqual(len(pd.pathmap), 0)
     for n,(method, path, function) in enumerate(paths):
