408f1a44be36d0d37675a2a1b6ea1e5dac3c3907
sjournea <sjournea@taralaur.com>
2018-03-26 01:25:25 +0000

    Added app_main.py

---
+++ app_main.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+"""app_main.py - golf app entry point."""
+import console
+import ui
+import dialogs
+from views.AppMain import MainView
+from golf_db.db_sqlalchemy import Database
+
+DB_URL = 'sqlite:///golf.sqlite'
+db = Database(DB_URL)
+
+class GolfApp(object):
+	def __init__(self):
+		self.view = ui.load_view('views/main')
+		self.view.db = db
+		self.view.setup()
+		self.view.present('sheet')
+		
+GolfApp()
+
