744893085c692a871c98f78adc4b4d2e02ce2ba2
Steve Journeay <sjournea@taralaur.com>
2018-03-25 20:15:20 -0700

    converted indentation

--- views/RoundCreate.py
+++ views/RoundCreate.py
@@ -8,112 +8,112 @@ import datetime
 from golf_db.db_sqlalchemy import Database, Round, Course, Player, Result
 
 class RoundCreate(ui.View):
-	def __init__(self):
-		pass
+  def __init__(self):
+    pass
 
-	def did_load(self):
-		self.name = "Let's Play Golf"
-		
-		self.btnSelectDate = self['btnSelectDate']
-		self.btnSelectCourse = self['btnSelectCourse']
-		self.btnSelectTee = self['btnSelectTee']
-		self.btnPlayers = [self['btnPlayer{}'.format(n+1)] for n in range(4)]
-		self.btnStartRound = self['btnStartRound']
-				
-		self.btnSelectDate.action = self.select_date
-		self._date_played = datetime.datetime.now()
-		self.btnSelectDate.title = self._date_played.strftime('%A %B %d, %Y')
-						
-		self.btnSelectCourse.action = self.select_course
-		self._course = None
-		self.btnSelectTee.action = self.select_tee
-		self.btnSelectTee.enabled = False
-		self._tee = None
-		
-		for player in self.btnPlayers:
-			player.action = self.add_player
-			player._player = None
-		
-		self.btnStartRound.action = self.start_round
+  def did_load(self):
+    self.name = "Let's Play Golf"
 
-	def activate(self):
-		pass
+    self.btnSelectDate = self['btnSelectDate']
+    self.btnSelectCourse = self['btnSelectCourse']
+    self.btnSelectTee = self['btnSelectTee']
+    self.btnPlayers = [self['btnPlayer{}'.format(n+1)] for n in range(4)]
+    self.btnStartRound = self['btnStartRound']
 
-	def select_date(self, sender):
-		date_played = dialogs.date_dialog()
-		if date_played:
-			sender.title = date_played.strftime('%A %B %d, %Y')
-			self._date_played = date_played
+    self.btnSelectDate.action = self.select_date
+    self._date_played = datetime.datetime.now()
+    self.btnSelectDate.title = self._date_played.strftime('%A %B %d, %Y')
 
-	def add_player(self, sender):
-		session = self.db.Session()
-		players = session.query(Player).all()
-		names = sorted([player.getFullName() for player in players])
-		names += ['<Remove Player>']
-		name = dialogs.list_dialog('Select Player', names)
-		if name:
-			if name == '<Remove Player>':
-				sender.title = '<Add Player>'
-				sender._player = None
-			else:
-				sender.title = name
-				sender._player = [player for player in players if name == player.getFullName()][0]
-	
-	def select_course(self, sender):
-		session = self.db.Session()
-		courses = session.query(Course).all()
-		course_names = sorted([course.name for course in courses])
-		name = dialogs.list_dialog('Select Course', course_names)
-		if name:
-			sender.title = name
-			self._course = [course for course in courses if name == course.name][0]
-			self._tee = None
-			self['btnSelectTee'].title = '<Select Tee>'			
-			self['btnSelectTee'].enabled = True
-			
-	def select_tee(self, sender):
-		if not self._course:
-			return
-		tees = [tee for tee in self._course.tees if tee.gender == 'mens']
-		names = [tee.name for tee in tees]
-		name = dialogs.list_dialog('Select Tee', names)
-		if name:
-			sender.title = name
-			self._tee = [tee for tee in tees if name == tee.name][0]
+    self.btnSelectCourse.action = self.select_course
+    self._course = None
+    self.btnSelectTee.action = self.select_tee
+    self.btnSelectTee.enabled = False
+    self._tee = None
 
-	@ui.in_background
-	def console_alert(self, msg):
-		console.alert(msg)
-				    
-	def start_round(self, sender):
-		lst_errors = []
-		if self._date_played is None:
-			lst_errors.append('Date played must be set.')
-		if self._course is None:
-			lst_errors.append('Course must be selected.')
-		if self._tee is None:
-			lst_errors.append('Tee must be selected.')
-		for player in self.btnPlayers:
-			if player._player is not None:
-				break
-		else:
-			lst_errors.append('At least one player must be selected.')
-		if lst_errors:
-			self.console_alert('\n'.join(lst_errors))
-			return
-		# Start the round
-		session = self.db.Session()
-		golf_round = Round(course_id=self._course.course_id, date_played=self._date_played)
-		session.add(golf_round)
-		session.commit()
-		# add players
-		for pl in self.btnPlayers:
-			if pl._player is not None:
-				result = Result(round=golf_round, player_id=pl._player.player_id, tee_id=self._tee.tee_id)
-				result.calcCourseHandicap(pl._player, self._tee)
-				session.add(result)
-		session.commit()
-		# switch back to main view
-		self._mainView.set_golf_round(golf_round)
-		self._mainView.goto_games()
+    for player in self.btnPlayers:
+      player.action = self.add_player
+      player._player = None
+
+    self.btnStartRound.action = self.start_round
+
+  def activate(self):
+    pass
+
+  def select_date(self, sender):
+    date_played = dialogs.date_dialog()
+    if date_played:
+      sender.title = date_played.strftime('%A %B %d, %Y')
+      self._date_played = date_played
+
+  def add_player(self, sender):
+    session = self.db.Session()
+    players = session.query(Player).all()
+    names = sorted([player.getFullName() for player in players])
+    names += ['<Remove Player>']
+    name = dialogs.list_dialog('Select Player', names)
+    if name:
+      if name == '<Remove Player>':
+        sender.title = '<Add Player>'
+        sender._player = None
+      else:
+        sender.title = name
+        sender._player = [player for player in players if name == player.getFullName()][0]
+
+  def select_course(self, sender):
+    session = self.db.Session()
+    courses = session.query(Course).all()
+    course_names = sorted([course.name for course in courses])
+    name = dialogs.list_dialog('Select Course', course_names)
+    if name:
+      sender.title = name
+      self._course = [course for course in courses if name == course.name][0]
+      self._tee = None
+      self['btnSelectTee'].title = '<Select Tee>'			
+      self['btnSelectTee'].enabled = True
+
+  def select_tee(self, sender):
+    if not self._course:
+      return
+    tees = [tee for tee in self._course.tees if tee.gender == 'mens']
+    names = [tee.name for tee in tees]
+    name = dialogs.list_dialog('Select Tee', names)
+    if name:
+      sender.title = name
+      self._tee = [tee for tee in tees if name == tee.name][0]
+
+  @ui.in_background
+  def console_alert(self, msg):
+    console.alert(msg)
+
+  def start_round(self, sender):
+    lst_errors = []
+    if self._date_played is None:
+      lst_errors.append('Date played must be set.')
+    if self._course is None:
+      lst_errors.append('Course must be selected.')
+    if self._tee is None:
+      lst_errors.append('Tee must be selected.')
+    for player in self.btnPlayers:
+      if player._player is not None:
+        break
+    else:
+      lst_errors.append('At least one player must be selected.')
+    if lst_errors:
+      self.console_alert('\n'.join(lst_errors))
+      return
+    # Start the round
+    session = self.db.Session()
+    golf_round = Round(course_id=self._course.course_id, date_played=self._date_played)
+    session.add(golf_round)
+    session.commit()
+    # add players
+    for pl in self.btnPlayers:
+      if pl._player is not None:
+        result = Result(round=golf_round, player_id=pl._player.player_id, tee_id=self._tee.tee_id)
+        result.calcCourseHandicap(pl._player, self._tee)
+        session.add(result)
+    session.commit()
+    # switch back to main view
+    self._mainView.set_golf_round(golf_round)
+    self._mainView.goto_games()
 
