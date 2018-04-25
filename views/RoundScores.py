961558a0cceebaa583d08031b06ba3620a939a81
sjournea <sjournea@taralaur.com>
2018-04-25 02:00:13 +0000

    Added dialog for inputing score and putts.

--- views/RoundScores.py
+++ views/RoundScores.py
@@ -10,145 +10,203 @@ from golf_db.exceptions import GolfGameException
 from golf_view import GolfView
 
 class PlayerForm:
-  def __init__(self, result_id, txtGross, txtPutts):
-    self.result_id = result_id
-    self.txtGross = txtGross
-    self.txtPutts = txtPutts
-    self.txtGross.keyboard_type = ui.KEYBOARD_NUMBER_PAD
-    self.txtPutts.keyboard_type = ui.KEYBOARD_NUMBER_PAD
+	def __init__(self, result_id, btnGross, btnPutts):
+		self.result_id = result_id
+		self.btnGross = btnGross
+		self.btnPutts = btnPutts
 
 class RoundScores(GolfView):
-  def __init__(self):
-    self._hole_num = 1
-    self._dirty = False
-    self.golf_round = None
+	def __init__(self):
+		self._hole_num = 1
+		self._dirty = False
+		self.golf_round = None
 
-  def did_load(self):
-    self.name = "Update Scores"
+	def did_load(self):
+		self.name = "Update Scores"
+		self.players = []
 
-    self.btnPrev = self['btnPrev']
-    self.btnNext = self['btnNext']
-    self.lblHole = self['lblHole']
-    self.btnNext.action = self.next_hole
-    self.btnPrev.action = self.prev_hole
-    self.lblPlayers = []
-    self.txtGross = []
-    self.txtPutts = []
-    for n in range(4):
-      self.lblPlayers.append(self['lblPlayer{}'.format(n+1)])
-      self.txtGross.append(self['txtGross{}'.format(n+1)])
-      self.txtPutts.append(self['txtPutts{}'.format(n+1)])
-    self.players = []
+		self.btnPrev = ui.Button(title='Prev')
+		self.btnPrev.center = (self.width * 0.1, self.height * 0.05)
+		self.btnPrev.flex = 'LRTB'
+		self.btnPrev.action = self.prev_hole
+		self.add_subview(self.btnPrev)
 
-  def deactivate(self):
-    print('{} deactivate()'.format(self.__class__.__name__))
-    if self.golf_round:
-      self._save()
+		self.lblHole = ui.Label(text='Hole Desc', center=(self.width * 0.5, self.height * 0.05), width=self.width*0.4)
+		self.add_subview(self.lblHole)
+	
+		self.btnNext = ui.Button(title='Next')
+		self.btnNext.center = (self.width * 0.9, self.height * 0.05)
+		self.btnNext.flex = 'LRTB'
+		self.btnNext.action = self.next_hole
+		self.add_subview(self.btnNext)
+	
+		self.lblPlayers = []
+		self.btnGross = []
+		self.btnPutts = []
+		for n in range(4):
+			height = self.height * 0.15 + n * self.height*0.10
+			lbl = ui.Label(text='Player {}'.format(n+1), center=(self.width*0.25, height), width=self.width*0.4)
+			self.add_subview(lbl)
+			self.lblPlayers.append(lbl)
 
-  def activate(self):
-    #print('{} activate()'.format(self.__class__.__name__))
-    for n in range(4):
-      self.lblPlayers[n].hidden = True
-      self.txtGross[n].hidden = True
-      self.txtPutts[n].hidden = True
+			btnGross = ui.Button(title='<not set>')
+			btnGross.center = (self.width * 0.5, height)
+			btnGross.flex = 'LRTB'
+			btnGross.action = self.set_gross
+			self.add_subview(btnGross)
+			self.btnGross.append(btnGross)
 
-    session = self.db.Session()
-    self.golf_round = session.query(Round).filter(Round.round_id == self._mainView._round_id).one()
-    self.players = []
-    for n, result in enumerate(self.golf_round.results):
-      self.lblPlayers[n].hidden = False
-      self.txtGross[n].hidden = False
-      self.txtPutts[n].hidden = False
-      self.lblPlayers[n].text = result.player.getFullName()
-      self.txtGross[n].action = self.set_gross
-      self.txtPutts[n].action = self.set_putts
-      self.players.append(PlayerForm(result.result_id, self.txtGross[n], self.txtPutts[n]))
+			btnPutts = ui.Button(title='<not set>')
+			btnPutts.center = (self.width * 0.7, height)
+			btnPutts.flex = 'LRTB'
+			btnPutts.action = self.set_putts
+			self.add_subview(btnPutts)
+			self.btnPutts.append(btnPutts)
 
-    self._set_hole_number(self._hole_num)
-    self._get(session)
+	def deactivate(self):
+		print('{} deactivate()'.format(self.__class__.__name__))
+		if self.golf_round:
+			self._save()
 
-  def _set_hole_number(self, hole_num):
-    #print('{} _set_hole_num() hole_num:{}'.format(self.__class__.__name__, hole_num))
-    hole_num = max(hole_num, 1)
-    hole_num = min(hole_num, len(self.golf_round.course.holes)) 
-    self.lblHole.text = 'Hole {} Par {} Hdcp {}'.format(hole_num,
-                                                                    self.golf_round.course.holes[hole_num-1].par,
-                    self.golf_round.course.holes[hole_num-1].handicap)
-    self._hole_num = hole_num
-    self.btnPrev.enabled = True
-    self.btnNext.enabled = True
-    if hole_num == 1:
-      self.btnPrev.enabled = False
-    elif hole_num == len(self.golf_round.course.holes):
-      self.btnNext.enabled = False
+	def activate(self):
+		#print('{} activate()'.format(self.__class__.__name__))
+		for n in range(4):
+			self.lblPlayers[n].hidden = True
+			self.btnGross[n].hidden = True
+			self.btnPutts[n].hidden = True
 
-  def _get(self, session):
-    """Read values from database and set into form"""
-    #print('{} _get() _hole_num:{}'.format(self.__class__.__name__, self._hole_num))
-    for player in self.players:
-      player.score = session.query(Score).filter(Score.result_id == player.result_id, Score.num == self._hole_num).first()
-      if player.score:
-        player.txtGross.text = str(player.score.gross)
-        player.txtPutts.text = str(player.score.putts)
-      else:
-        player.txtGross.text = ''
-        player.txtPutts.text = ''
+		session = self.db.Session()
+		self.golf_round = session.query(Round).filter(Round.round_id == self._mainView._round_id).one()
+		self.players = []
+		for n, result in enumerate(self.golf_round.results):
+			self.lblPlayers[n].hidden = False
+			self.btnGross[n].hidden = False
+			self.btnPutts[n].hidden = False
+			self.lblPlayers[n].text = result.player.getFullName()
+			self.players.append(PlayerForm(result.result_id, self.btnGross[n], self.btnPutts[n]))
 
-  def _save(self, session=None):
-    """Save the from values to the database."""
-    print('{} _save() _hole_num:{}'.format(self.__class__.__name__, self._hole_num))
-    if not session:
-      session = self.db.Session()
-    for player in self.players:
-      try:
-        gross = int(player.txtGross.text)
-        putts = int(player.txtPutts.text)
-      except Exception, ex:
-        print('_save() error - {}'.format(ex))
-        continue
-      player.score = session.query(Score).filter(Score.result_id == player.result_id, Score.num == self._hole_num).first()
-      if player.score:
-        player.score.gross = gross
-        player.score.putts = putts
-      else:
-        player.score = Score(result_id=player.result_id, gross=gross, putts=putts, num=self._hole_num)
-        session.add(player.score)
-    session.commit()
-    # now validate scores
-    lst_game_more_info_needed = []
-    golf_round = session.query(Round).filter(Round.round_id == self._mainView._round_id).one()
-    for game in golf_round.games:
-      try:
-        game.CreateGame()
-      except GolfGameException, ex:
-        print('{} Game - {} - {}'.format(ex.dct['game'].short_description, ex.dct['msg'], ','.join([pl.nick_name for pl in ex.dct['players']])))
-        lst_game_more_info_needed.append(ex)
-      
-    if lst_game_more_info_needed:
-      for ex in lst_game_more_info_needed:
-        player_nick_names = [pl.nick_name for pl in ex.dct['players']]
-        name = dialogs.list_dialog(ex.dct['msg'], player_nick_names)
-        if name:
-          game = session.query(Game).filter(Game.game_id == ex.dct['game'].game.game_id).one()
-          game.add_hole_dict_data(ex.dct['hole_num'], {ex.dct['key'] : name})
-          session.commit()
+		self._set_hole_number(self._hole_num)
+		self._get(session)
 
-  def next_hole(self, sender):
-    session = self.db.Session()
-    self._save(session)
-    self._set_hole_number(self._hole_num + 1)
-    self._get(session)		
+	def _set_hole_number(self, hole_num):
+		#print('{} _set_hole_num() hole_num:{}'.format(self.__class__.__name__, hole_num))
+		hole_num = max(hole_num, 1)
+		hole_num = min(hole_num, len(self.golf_round.course.holes)) 
+		self._current_hole = self.golf_round.course.holes[hole_num-1]
+		self.lblHole.text = 'Hole {} Par {} Hdcp {}'.format(
+			hole_num, self.golf_round.course.holes[hole_num-1].par,
+			self.golf_round.course.holes[hole_num-1].handicap)
+		self._hole_num = hole_num
+		self.btnPrev.enabled = hole_num > 1
+		self.btnNext.enabled = hole_num < len(self.golf_round.course.holes)
 
-  def prev_hole(self, sender):
-    session = self.db.Session()
-    self._save(session)
-    self._set_hole_number(self._hole_num - 1)
-    self._get(session)
+	def _get(self, session):
+		"""Read values from database and set into form"""
+		print('{} _get() _hole_num:{}'.format(self.__class__.__name__, self._hole_num))
+		for player in self.players:
+			score = session.query(Score).filter(Score.result_id == player.result_id, Score.num == self._hole_num).first()
+			#print('_get() score:{}'.format(score))
+			if score:
+				#print('_get() score.gross:{}'.format(score.gross))
+				player.btnGross.title = str(score.gross)
+				player.btnPutts.title = str(score.putts)
+			else:
+				player.btnGross.title = '<not set>'
+				player.btnPutts.title = '<not set>'
 
-  def set_gross(self, sender):
-    pass
+	def _save(self, session=None):
+		"""Save the from values to the database."""
+		print('{} _save() _hole_num:{}'.format(self.__class__.__name__, self._hole_num))
+		if not session:
+			session = self.db.Session()
+		for player in self.players:
+			try:
+				gross = int(player.btnGross.title)
+				putts = int(player.btnPutts.title)
+			except Exception, ex:
+				print('_save() error - {}'.format(ex))
+				continue
+			score = session.query(Score).filter(Score.result_id == player.result_id, Score.num == self._hole_num).first()
+			if score:
+				score.gross = gross
+				score.putts = putts
+			else:
+ 				score = Score(result_id=player.result_id, gross=gross, putts=putts, num=self._hole_num)
+ 				session.add(score)
+		session.commit()
+		# now validate scores
+		lst_game_more_info_needed = []
+		golf_round = session.query(Round).filter(Round.round_id == self._mainView._round_id).one()
+		for game in golf_round.games:
+			try:
+				game.CreateGame()
+			except GolfGameException, ex:
+				print('{} Game - {} - {}'.format(ex.dct['game'].short_description, ex.dct['msg'], ','.join([pl.nick_name for pl in ex.dct['players']])))
+				lst_game_more_info_needed.append(ex)
 
-  def set_putts(self, sender):
-    pass
+		if lst_game_more_info_needed:
+			for ex in lst_game_more_info_needed:
+				player_nick_names = [pl.nick_name for pl in ex.dct['players']]
+				name = dialogs.list_dialog(ex.dct['msg'], player_nick_names)
+				if name:
+					game = session.query(Game).filter(Game.game_id == ex.dct['game'].game.game_id).one()
+					game.add_hole_dict_data(ex.dct['hole_num'], {ex.dct['key'] : name})
+			session.commit()
+
+	def next_hole(self, sender):
+		session = self.db.Session()
+		self._save(session)
+		self._set_hole_number(self._hole_num + 1)
+		self._get(session)		
+
+	def prev_hole(self, sender):
+		session = self.db.Session()
+		self._save(session)
+		self._set_hole_number(self._hole_num - 1)
+		self._get(session)
+
+	def set_gross(self, sender):
+		par_3_scores = [
+			'1 - Ace',
+			'2 - Birdie',
+			'3 - Par',
+			'4 - Bogey',
+			'5 - Double Bogey',
+			'6 - Triple Bogey',
+		]
+		par_4_scores = [
+			'1 - Ace',
+			'2 - Eagle',
+			'3 - Birdie',
+			'4 - Par',
+			'5 - Bogey',
+			'6 - Double Bogey',
+			'7 - Triple Bogey',
+		]	
+		par_5_scores = [
+			'1 - Ace',
+			'2 - Double Eagle',
+			'3 - Eagle',
+			'4 - Birdie',
+			'5 - Par',
+			'6 - Bogey',
+			'7 - Double Bogey',
+			'8 - Triple Bogey',
+		]
+		dct_scores = {
+			3: par_3_scores + [str(n) for n in range(7,21)],
+			4: par_4_scores + [str(n) for n in range(8,21)],
+			5: par_5_scores + [str(n) for n in range(9,21)],		
+		}
+		title = 'Hole {} Par {} - Gross'.format(self._current_hole.num, self._current_hole.par)
+		gross = dialogs.list_dialog(title, dct_scores[self._current_hole.par])
+		if gross:
+			sender.title = gross.split()[0]
+
+	def set_putts(self, sender):
+		putt_scores = [str(n) for n in range(11)]
+		putts = dialogs.list_dialog('Hole 1 Par 3 - Putts', putt_scores)
+		if putts:
+			sender.title = putts
 
 
